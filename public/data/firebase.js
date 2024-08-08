b2a6bfbb6e22316bf24f36e6e138ef3c328a1f33
tjf1 <117757363+tjf1dev@users.noreply.github.com>
2024-08-08 17:34:32 +0200

    removed old files

--- public/data/firebase.js
+++
@@ -1,5 +0,0 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-analytics.js";
-const firebaseConfig = ${{ secrets.firebase }}
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
